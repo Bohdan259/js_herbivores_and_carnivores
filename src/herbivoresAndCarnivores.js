5c11500ef2a948e4685897c3b0c71061549f96ed
Bohdan Pidhrebelnyi <bodiiiiic@gmail.com>
2025-09-28 14:17:47 +0100

    fixed comments

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -32,13 +32,7 @@ class Carnivore extends Animal {
 
     prey.health -= 50;
 
-    if (prey.health <= 0) {
-      const index = Animal.alive.indexOf(prey);
-
-      if (index > -1) {
-        Animal.alive.splice(index, 1);
-      }
-    }
+    Animal.alive = Animal.alive.filter((a) => a.health > 0);
   }
 }
 
